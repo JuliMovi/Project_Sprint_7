82571fb4e53c605e77054cdd357fcfdbf6cbc6f7
JuliMovi <julimovi04@hotmail.com>
2025-07-12 15:40:32 +0200

    corrigiendo graficas

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ disp_button = st.button('Construir grafico de dispersion') # crear un botón
 if disp_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un grafico de dispersion')
-    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
-    fig.show() # crear gráfico de dispersión
+    fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_2, use_container_width=True)
